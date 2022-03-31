1d81cc3bc514fed9a70ad24af379445b3cedd971
Sampada Jain <jain.sampada@gmail.com>
2022-03-31 14:06:53 +0530

    Version2 and Version3 Squashed

--- src/test/java/com/BaseTest/TestBase.java
+++ src/test/java/com/BaseTest/TestBase.java
@@ -12,44 +12,42 @@ import org.openqa.selenium.interactions.Actions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
-public static WebDriver driver;
-public static Properties prop;
-public static String path;
+    public static WebDriver driver;
+    public static Properties prop;
+    public static String path;
 
-public TestBase()
-{
-	try
-	{
-		//Setting path of properties file
-		path=System.getProperty("user.dir")+"//src//test//java//com//configuration//configuration.properties";
-		
-		//Read data from properties file
-		FileInputStream ip=new FileInputStream(path);
-		
-		//Initializing the properties file 
-		prop=new Properties();
-		
-		prop.load(ip);
-		
-	}
-	catch(IOException e)
-	{
-		e.printStackTrace();
-	}
-}
-public static void LaunchDriver()
-{
-	//Setting up Chrome Driver
-	WebDriverManager.chromedriver().setup();
-	
-	//Initializing Chrome Driver
-	driver=new ChromeDriver();
-	
-	//Setting Implicit wait for 5 seconds
-	driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-	driver.manage().deleteAllCookies();
-	driver.manage().window().maximize();
-	driver.get(prop.getProperty("url"));
-	
-}
+    public TestBase() {
+        try {
+            //Setting path of properties file
+            path = System.getProperty("user.dir") + "//src//test//java//com//configuration//configuration.properties";
+
+            //Read data from properties file
+            FileInputStream ip = new FileInputStream(path);
+
+            //Initializing the properties file
+            prop = new Properties();
+
+            prop.load(ip);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void LaunchDriver() {
+        //Setting up Chrome Driver
+        WebDriverManager.chromedriver().setup();
+
+        //Initializing Chrome Driver
+        driver = new ChromeDriver();
+
+        //Setting Implicit wait for 5 seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.get(prop.getProperty("url"));
+
+    }
 }
--- src/test/java/com/Pages/BuyProduct.java
+++ src/test/java/com/Pages/BuyProduct.java
@@ -9,37 +9,38 @@ import org.openqa.selenium.support.PageFactory;
 import com.BaseTest.TestBase;
 
 public class BuyProduct extends TestBase {
-	 Actions act =  new Actions(driver);
-	@FindBy(linkText = "Mobiles")
-	WebElement category;
+    Actions act = new Actions(driver);
+	//Locator Info
+    @FindBy(linkText = "Mobiles")
+    WebElement category;
 
-	@FindBy(xpath = "//div[text()='IQOO Z6']")
-	WebElement product;
+    @FindBy(xpath = "//div[text()='IQOO Z6']")
+    WebElement product;
 
-	@FindBy(xpath = "//span[text()='Add to Cart']")
-	WebElement addtocart;
+    @FindBy(xpath = "//span[text()='Add to Cart']")
+    WebElement addtocart;
 
-	@FindBy(id = "attach-sidesheet-checkout-button")
-	WebElement checkout;
+    @FindBy(id = "attach-sidesheet-checkout-button")
+    WebElement checkout;
 
-	public BuyProduct() {
-		PageFactory.initElements(driver, this);
-	}
+    public BuyProduct() {
+        PageFactory.initElements(driver, this);
+    }
 
-	public String VerifyTitle() {
-		return driver.getCurrentUrl();
-	}
+    public String VerifyTitle() {
+        return driver.getCurrentUrl();
+    }
 
-	public void Purchase() {
+    public void Purchase() {
 
-		category.click();
-		product.click();
-		act.moveToElement(addtocart).click().perform();
-		checkout.click();
+        category.click();
+        product.click();
+        act.moveToElement(addtocart).click().perform();
+        checkout.click();
 
-	}
+    }
 
-	public void CloseBrowser() {
-		driver.quit();
-	}
+    public void CloseBrowser() {
+        driver.quit();
+    }
 }
